e397ae641c5a5cd01463339754212c801226edd1
Paul Garcia <paul.ngarcia@yahoo.com>
2023-05-18 10:15:33 -0500

    fixed dice

--- src/main/java/com/codeup/codeupspringblog/controllers/rolldice.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/rolldice.java
@@ -5,44 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @Controller
 public class rolldice {
     @GetMapping("/roll-dice")
-    public String rollDice(@RequestParam(required = false) List<Integer> guess, Model model) {
-        List<Integer> diceRolls = new ArrayList<>();
-
-        if (guess != null && !guess.isEmpty()) {
+    public String rollDice(@RequestParam(required = false) Integer guess, Model model) {
+        if (guess != null && guess >= 1 && guess <= 6) {
             Random random = new Random();
+            int diceRoll = random.nextInt(6) + 1;
 
-            for (int i = 0; i < guess.size(); i++) {
-                int diceRoll = random.nextInt(6) + 1;
-                diceRolls.add(diceRoll);
-            }
+            model.addAttribute("guess", guess);
+            model.addAttribute("diceRoll", diceRoll);
+            model.addAttribute("isCorrect", guess.equals(diceRoll));
         }
-
-        model.addAttribute("guess", guess);
-        model.addAttribute("diceRolls", diceRolls);
-        model.addAttribute("isCorrect", checkGuesses(guess, diceRolls));
 
         return "roll-dice";
-    }
-
-    private List<Boolean> checkGuesses(List<Integer> guess, List<Integer> diceRolls) {
-        List<Boolean> isCorrectList = new ArrayList<>();
-
-        if (guess != null && diceRolls != null) {
-            for (int i = 0; i < guess.size(); i++) {
-                int guessNumber = guess.get(i);
-                int diceRoll = diceRolls.get(i);
-                boolean isCorrect = guessNumber == diceRoll;
-                isCorrectList.add(isCorrect);
-            }
-        }
-
-        return isCorrectList;
     }
 }
