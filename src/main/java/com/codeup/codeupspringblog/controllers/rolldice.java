8ed396d7d33989545f7d2e201d28142fe5e09148
Paul Garcia <paul.ngarcia@yahoo.com>
2023-05-18 10:03:11 -0500

    broke dice but will fix later

--- src/main/java/com/codeup/codeupspringblog/controllers/rolldice.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/rolldice.java
@@ -1,28 +1,46 @@
-package com.codeup.codeupspringblog.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Controller
 public class rolldice {
     @GetMapping("/roll-dice")
-    public String dice() {
+    public String rollDice(@RequestParam(required = false) List<Integer> guess, Model model) {
+        List<Integer> diceRolls = new ArrayList<>();
+
+        if (guess != null && !guess.isEmpty()) {
+            Random random = new Random();
+
+            for (int i = 0; i < guess.size(); i++) {
+                int diceRoll = random.nextInt(6) + 1;
+                diceRolls.add(diceRoll);
+            }
+        }
+
+        model.addAttribute("guess", guess);
+        model.addAttribute("diceRolls", diceRolls);
+        model.addAttribute("isCorrect", checkGuesses(guess, diceRolls));
+
         return "roll-dice";
     }
 
-    @GetMapping("/roll-dice/{number}")
-    public String rollDice(@PathVariable int number, Model model) {
-        Random random = new Random();
-        int diceRoll = random.nextInt(6) + 1;
+    private List<Boolean> checkGuesses(List<Integer> guess, List<Integer> diceRolls) {
+        List<Boolean> isCorrectList = new ArrayList<>();
 
-        model.addAttribute("number", number);
-        model.addAttribute("diceRoll", diceRoll);
-        model.addAttribute("isCorrect", number == diceRoll);
+        if (guess != null && diceRolls != null) {
+            for (int i = 0; i < guess.size(); i++) {
+                int guessNumber = guess.get(i);
+                int diceRoll = diceRolls.get(i);
+                boolean isCorrect = guessNumber == diceRoll;
+                isCorrectList.add(isCorrect);
+            }
+        }
 
-        return "result-page";
+        return isCorrectList;
     }
 }
